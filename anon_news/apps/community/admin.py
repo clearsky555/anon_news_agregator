ba3f5cac3c82db138c998cf56b8b612f25fdef23
clear <clarence.sky@yabdex.ru>
2023-08-21 19:23:52 +0600

    work with admin panel

--- anon_news/apps/community/admin.py
+++ anon_news/apps/community/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from apps.community.models import Community
+
+
 # Register your models here.
+@admin.register(Community)
+class CommunityAdmin(admin.ModelAdmin):
+    list_display = [
+        'creator',
+        'title',
+        'slug',
+        'description',
+        'is_private',
+    ]
--- anon_news/config/settings.py
+++ anon_news/config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os.path
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!ah=(6chxb6#_d2k_j3lv(n86b+!c!u-*v##u_o3^9ly=pap3+'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
